d2b46ab8338385599717bcc8e4e6c136f99be4e0
Tino Muzambi <tinomuzambi@gmail.com>
2022-01-02 13:10:54 +0200

    Adds colour styles and markup.

--- components/Colour.tsx
+++ components/Colour.tsx
@@ -1,8 +1,22 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { ColourProps } from "../utils/interfaces";
 
 const Colour: React.FC<ColourProps> = ({ hex, name }) => {
-	return <View></View>;
+	return (
+		<View style={styles.colour}>
+			<Text style={styles.colourText}>{name}</Text>
+		</View>
+	);
 };
 
 export default Colour;
+
+const styles = StyleSheet.create({
+	colour: {
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	colourText: {
+		color: "white",
+	},
+});
